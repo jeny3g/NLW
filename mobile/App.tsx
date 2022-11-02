bb421434bfe6f6a72a4ea6156a250b75570eb4ef
Jean Oliveira Miranda <mirandajean2009@gmail.com>
2022-11-02 01:29:52 -0300

    feat: :sparkles: installing fonts and making sure that isLoaded

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,16 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NativeBaseProvider, Box } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 import { THEME } from "./src/styles/theme";
+import { Loading } from "./components/Loading";
+import { SignIn } from "./screens/SignIn";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
