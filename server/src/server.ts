a446a4befe7aa26a7386c46c11bc86dc8b847c84
Jean Oliveira Miranda <mirandajean2009@gmail.com>
2022-11-02 14:25:14 -0300

    feat: :sparkles: creating new pool

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 import cors from "@fastify/cors";
 import Fastify from "fastify";
+import { z } from "zod";
+import ShortUniqueId from "short-unique-id";
 
 const prisma = new PrismaClient({
   log: ["query"],
@@ -19,6 +21,26 @@ async function bootstrap() {
     const count = await prisma.pool.count();
 
     return { count };
+  });
+
+  app.post("/pools", async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    });
+
+    const { title } = createPoolBody.parse(request.body);
+
+    const generate = new ShortUniqueId({ length: 6 });
+    const code = String(generate()).toUpperCase();
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    return reply.status(201).send({ code });
   });
 
   await app.listen({ port: 3333, host: "0.0.0.0" });
